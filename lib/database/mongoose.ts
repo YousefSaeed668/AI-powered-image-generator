68aee8bf91ab0a3a337f19d58864bc14dc2c47a0
Yousef Saeed <yousefsaeed668@gmail.com>
2024-02-21 15:17:18 +0200

    edit mongoose

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,24 +7,28 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cashed: MongooseConnection = (global as any).mongoose;
+let cached: MongooseConnection = (global as any).mongoose;
 
-if (!cashed) {
-  cashed = (global as any).mongoose = { conn: null, promise: null };
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
 export const connectToDatabase = async () => {
-  if (cashed.conn) return cashed.conn;
+  if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) {
-    throw new Error("Missing MONGODB_URL");
-  }
-  cashed.promise =
-    cashed.promise ||
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+
+  cached.promise =
+    cached.promise ||
     mongoose.connect(MONGODB_URL, {
       dbName: "imaginify",
       bufferCommands: false,
     });
-  cashed.conn = await cashed.promise;
-  return cashed.conn;
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };
